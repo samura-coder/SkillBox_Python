8e07b24277a0cfa4b0bd5357190bc4136c7a9dff
samura-coder <abutkovs@gmail.com>
2023-05-31 13:36:15 +0300

    lesson_002: 00_distance.py - выполнено.

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -14,10 +14,24 @@ sites = {
 
 distances = {}
 
-# TODO здесь заполнение словаря
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
+
+moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2) ** 0.5
+moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2) ** 0.5
+london_paris = ((london[0] - paris[0]) ** 2 + (london[1] - paris[1]) ** 2) ** 0.5
+
+distances['Moscow'] = {}
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
+
+distances['London'] = {}
+distances['London']['Paris'] = london_paris
+distances['London']['Moscow'] = moscow_london
+
+distances['Paris'] = {}
+distances['Paris']['Moscow'] = moscow_paris
+distances['Paris']['London'] = london_paris
 
 print(distances)
-
-
-
-
